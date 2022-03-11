9f43860540f1acbdfeae76765ac8e9b4c641b330
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-03-11 17:44:42 +0000

    Contract added

--- index.mjs
+++ index.mjs
@@ -1,27 +1,33 @@
-import {loadStdlib} from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+import { loadStdlib } from "@reach-sh/stdlib";
+import * as backend from "./build/index.main.mjs";
 const stdlib = loadStdlib(process.env);
 
-  const startingBalance = stdlib.parseCurrency(100);
+const startingBalance = stdlib.parseCurrency(100);
 
-  const [ accAlice, accBob ] =
-    await stdlib.newTestAccounts(2, startingBalance);
-  console.log('Hello, Alice and Bob!');
+const [accAlice, accBob] = await stdlib.newTestAccounts(2, startingBalance);
+console.log("Hello, Alice and Bob!");
 
-  console.log('Launching...');
-  const ctcAlice = accAlice.contract(backend);
-  const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+console.log("Launching...");
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-  console.log('Starting backends...');
-  await Promise.all([
-    backend.Alice(ctcAlice, {
-      ...stdlib.hasRandom,
-      // implement Alice's interact object here
-    }),
-    backend.Bob(ctcBob, {
-      ...stdlib.hasRandom,
-      // implement Bob's interact object here
-    }),
-  ]);
-
-  console.log('Goodbye, Alice and Bob!');
+const HAND = ["Rock", "Paper", "Scissors"];
+const OUTCOME = ["Bob wins", "Draw", "Alice wins"];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
+await Promise.all([
+  ctcAlice.p.Alice({
+    ...Player("Alice"),
+  }),
+  ctcBob.p.Bob({
+    ...Player("Bob"),
+  }),
+]);
